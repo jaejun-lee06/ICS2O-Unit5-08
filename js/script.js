519d37a9fd56c7565b461fba59588b8557da2923
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-22 02:48:38 +0000

    js pwa fix

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@
 // This file contains the JS functions for index.html
 
 
+/**
+ * Check servie worker.
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-08/sw.js", {
+    scope: "/ICS2O-Unit5-08/",
+  });
+}
+
 'use strict'
 
 /**
